3b47564365b4d13cb12430731493d3dbe775e491
Jacob Wisnoff <jacobwisnoff@gmail.com>
2025-04-15 21:50:35 -0400

    testing bstmap

--- src/week9/Week9Driver.java
+++ src/week9/Week9Driver.java
@@ -7,8 +7,11 @@ public class Week9Driver {
         BSTMap<Integer,String> map = new BSTMap<>();
         map.put(1, "Bat");
         map.put(1, "Rat");
+        map.put(1, "Dog");
 
         System.out.println(map.get(1));
+        System.out.println(map.size());
+        map.remove(1);
 
     }
 }

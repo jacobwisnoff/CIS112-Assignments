e4b439ae7fee819c743ce3367fcd13784abe7a62
Jacob Wisnoff <jacobwisnoff@gmail.com>
2025-04-29 23:12:20 -0400

    final project

---
+++ src/MergeLinkedLists/MergeSortedLL.java
@@ -0,0 +1,16 @@
+package MergeLinkedLists;
+
+import ch06.lists.*;
+import support.LLNode;
+
+public class MergeSortedLL {
+
+    public static void main(String[] args){
+
+        public SortedLL<T> merge(){
+
+        }
+
+    }
+
+}
---
+++ src/MergeLinkedLists/SortedLL.java
@@ -0,0 +1,76 @@
+package MergeLinkedLists;
+
+import ch06.lists.*;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class SortedLL<T> implements ListInterface<T> {
+
+    public void add(int index, T element) {
+        
+    }
+
+
+    public T set(int index, T newElement) {
+        return null;
+    }
+
+
+    public T get(int index) {
+        return null;
+    }
+
+
+    public int indexOf(T target) {
+        return 0;
+    }
+
+
+    public T remove(int index) {
+        return null;
+    }
+
+    
+    public boolean add(T element) {
+        return false;
+    }
+
+    
+    public T get(T target) {
+        return null;
+    }
+
+    
+    public boolean contains(T target) {
+        return false;
+    }
+
+    
+    public boolean remove(T target) {
+        return false;
+    }
+
+    
+    public boolean isFull() {
+        return false;
+    }
+
+    
+    public boolean isEmpty() {
+        return false;
+    }
+
+    
+    public int size() {
+        return 0;
+    }
+
+    
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+
+}
